7129b18befd7978ab59a9b9d0830671b2bf6a3d8
Saint-lawal <abiso.lawal@gmail.com>
2018-07-29 15:46:33 +0100

    - Rename folder

--- public/js/shared/Filters/FirstCapital.js
+++ public/js/shared/Filters/FirstCapital.js
@@ -1,5 +1,6 @@
 angular.module('FirstCapitalFilter', []).filter('capital', function() {
   return function(value) {
+    // Relace first letter
     const output = value.charAt(0).toUpperCase() + value.slice(1);
     return output;
   }
