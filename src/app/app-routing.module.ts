98cf214cd287e16785a97f860eda0f237210fa0a
Saint-lawal <abiso.lawal@gmail.com>
2019-03-09 20:55:44 +0100

    -Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 const routes: Routes = [
   {
